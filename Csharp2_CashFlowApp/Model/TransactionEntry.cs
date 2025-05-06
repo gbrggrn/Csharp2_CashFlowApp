ca78c7e09d2558f360caea379651e4d9fd3a33e0
gbrggrn <gustberg107@student.kau.se>
2025-05-06 13:13:05 +0200

    Initial commit. Basic file structure

---
+++ Csharp2_CashFlowApp/Model/TransactionEntry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp2_CashFlowApp.Model
+{
+    internal class TransactionEntry
+    {
+        public record Transaction
+        {
+
+        }
+    }
+}
