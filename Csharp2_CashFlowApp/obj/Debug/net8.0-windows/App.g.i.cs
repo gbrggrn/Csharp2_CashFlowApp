a7c4318fdf0ead65ffb66ff14ae7febdb4ecb718
gbrggrn <gustberg107@student.kau.se>
2026-02-20 14:18:51 +0100

    Final commmit

--- Csharp2_CashFlowApp/obj/Debug/net8.0-windows/App.g.i.cs
+++ Csharp2_CashFlowApp/obj/Debug/net8.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace Csharp2_CashFlowApp {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.0.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace Csharp2_CashFlowApp {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.0.0")]
         public static void Main() {
             Csharp2_CashFlowApp.App app = new Csharp2_CashFlowApp.App();
             app.InitializeComponent();
